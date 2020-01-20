02de39265fca9a9cab66db4df2712bccb9336078
JKostov <julijekos@gmail.com>
2020-01-20 19:46:59 +0100

    Move code

--- linux-5.4.9/include/linux/sched.h
+++ linux-5.4.9/include/linux/sched.h
@@ -197,10 +197,10 @@ struct task_group;
 
 
 /** NOS-EXTENSION */
-// #define __set_current_state(state_value) ({ current->state = (state_value); add_new_state_in_state_changes(state_value); })
+#define __set_current_state(state_value) ({ current->state = (state_value); add_new_state_in_state_changes(state_value); })
 
 /** NOS-EXTENSION */
-// #define set_current_state(state_value) ({ smp_store_mb(current->state, (state_value)); add_new_state_in_state_changes(state_value); })
+#define set_current_state(state_value) ({ smp_store_mb(current->state, (state_value)); add_new_state_in_state_changes(state_value); })
 
 /*
  * set_special_state() should be used for those states when the blocking task
