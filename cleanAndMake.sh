769db28b841f99526c56dc905da4e9dd929d764e
nos <julijekos@gmail.com>
2020-01-21 19:44:45 +0100

    scripts fix

--- cleanAndMake.sh
+++ cleanAndMake.sh
@@ -1,11 +1,11 @@
 cd ~/Projects/nos-1/
-sudo rm linux-5.4.9-*
+sudo rm linux-5.4.9-state*
 
-sudo apt-get --purge remove linux-image-5.4.9-states
-sudo apt-get --purge remove linux-headers-5.4.9-states
+sudo apt-get --purge remove linux-image-5.4.9-state
+sudo apt-get --purge remove linux-headers-5.4.9-state
 
 git pull origin master
 
 cd linux-5.4.9
 make clean
-make deb-pkg
+# make deb-pkg
--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 cd ~/Projects/nos-1
 sudo dpkg -i linux-image-5.4.9*.deb
 sudo dpkg -i linux-headers-5.4.9*.deb
-reboot
