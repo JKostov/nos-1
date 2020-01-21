7efcbd64ad9bbf88638cebe4a0a6443ed952d94b
nos <julijekos@gmail.com>
2020-01-21 19:48:35 +0100

    fix

--- cleanAndMake.sh
+++ cleanAndMake.sh
@@ -1,5 +1,8 @@
 cd ~/Projects/nos-1/
 sudo rm linux-5.4.9-state*
+sudo rm linux-image-5.4.9-state*
+sudo rm linux-headers-5.4.9-state*
+sudo rm linux-libc-dev_5.4.9-state*
 
 sudo apt-get --purge remove linux-image-5.4.9-state
 sudo apt-get --purge remove linux-headers-5.4.9-state
