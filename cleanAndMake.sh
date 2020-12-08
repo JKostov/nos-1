19a04ec3699da5582943c68dcc1e661400c2c102
JKostov <julijekos@gmail.com>
2020-12-08 22:44:35 +0100

    Fix clean script

--- cleanAndMake.sh
+++ cleanAndMake.sh
@@ -1,11 +1,11 @@
 cd ~/Projects/nos-1/
-sudo rm linux-5.4.9-state*
-sudo rm linux-image-5.4.9-state*
-sudo rm linux-headers-5.4.9-state*
-sudo rm linux-libc-dev_5.4.9-state*
+sudo rm linux-5.4.9*
+sudo rm linux-image-5.4.9*
+sudo rm linux-headers-5.4.9*
+sudo rm linux-libc-dev_5.4.9*
 
-sudo apt-get --purge remove linux-image-5.4.9-state
-sudo apt-get --purge remove linux-headers-5.4.9-state
+sudo apt-get --purge remove linux-image-5.4.9
+sudo apt-get --purge remove linux-headers-5.4.9
 
 git pull
 
