1c2ad20cc5284315e959553f279454252d913daf
JKostov <julijekos@gmail.com>
2020-12-08 21:08:13 +0100

    Clean start

---
+++ cleanAndMake.sh
@@ -0,0 +1,14 @@
+cd ~/Projects/nos-1/
+sudo rm linux-5.4.9-state*
+sudo rm linux-image-5.4.9-state*
+sudo rm linux-headers-5.4.9-state*
+sudo rm linux-libc-dev_5.4.9-state*
+
+sudo apt-get --purge remove linux-image-5.4.9-state
+sudo apt-get --purge remove linux-headers-5.4.9-state
+
+git pull origin master
+
+cd linux-5.4.9
+make clean
+# make deb-pkg
