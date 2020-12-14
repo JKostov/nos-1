454300bf3d88654e74f71cd71886ad7610824a7e
JKostov <julijekos@gmail.com>
2020-12-14 18:47:44 +0100

    Raw spin lock

--- linux-5.4.9/include/linux/sched.h
+++ linux-5.4.9/include/linux/sched.h
@@ -2057,14 +2057,14 @@ static inline void add_new_state_in_state_changes(void)
 
 	if (!is_idle_task(p))
 	{
-		spin_lock(&p->alloc_lock);
-		// raw_spin_lock(&p->pi_lock);
+		// spin_lock(&p->alloc_lock);
+		raw_spin_lock(&p->pi_lock);
 
 		p->number_of_state_changes = p->number_of_state_changes + 1;
 		add_state_change();
 
-		// raw_spin_unlock(&p->pi_lock);
-		spin_unlock(&p->alloc_lock);
+		raw_spin_unlock(&p->pi_lock);
+		// spin_unlock(&p->alloc_lock);
 	}
 }
 #endif
