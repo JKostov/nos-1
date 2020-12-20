8bac671c7d566be2d848b075ddac3507161eb7d4
JKostov <julijekos@gmail.com>
2020-12-20 15:11:02 +0100

    Add miliseconds param

--- linux-5.4.9/include/linux/syscalls.h
+++ linux-5.4.9/include/linux/syscalls.h
@@ -1424,4 +1424,4 @@ long compat_ksys_semtimedop(int semid, struct sembuf __user *tsems,
 #endif
 
 asmlinkage long sys_hello(void);
-asmlinkage long print_state_changes(pid_t pid);
+asmlinkage long print_state_changes(pid_t pid, int milliseconds);
--- linux-5.4.9/kernel/state_changes.c
+++ linux-5.4.9/kernel/state_changes.c
@@ -3,8 +3,8 @@
 #include <linux/list.h>
 #include <linux/syscalls.h>
 
-SYSCALL_DEFINE1(print_state_changes, pid_t, pid)
+SYSCALL_DEFINE2(print_state_changes, pid_t, pid, int, milliseconds)
 {
-  printk("SYSCALL print state changes for porcess pid: %d\n", pid);
+  printk("SYSCALL print state changes for process with pid: %d\n int the last %d miliseconds", pid, milliseconds);
   return 0;
 }
