3d679bd6d463ffa5a1f2eb1749b24dd964dc1fca
JKostov <julijekos@gmail.com>
2020-12-21 17:56:14 +0100

    Fix time

--- linux-5.4.9/kernel/state_changes.c
+++ linux-5.4.9/kernel/state_changes.c
@@ -4,8 +4,20 @@
 #include <linux/syscalls.h>
 #include <linux/timekeeping.h>
 
-static void print_states(struct task_struct *p, unsigned int milliseconds)
+SYSCALL_DEFINE2(print_state_changes, pid_t, pid, int, milliseconds)
 {
+  struct task_struct *p;
+  printk("SYSCALL print state changes for process with pid: %d, for the last %d miliseconds\n", pid, milliseconds);
+
+  p = find_task_by_vpid(pid);
+
+  if (p == NULL)
+  {
+    printk("Unable to find process with pid: %d\n", pid);
+    return -1;
+    
+  }
+
   struct state_change *state_changes;
   u64 time_ns = ktime_get_real();
   u64 max_time = time_ns - milliseconds * 1000000;
@@ -27,23 +39,6 @@ static void print_states(struct task_struct *p, unsigned int milliseconds)
   }
 
   printk("\n");
-}
-
-SYSCALL_DEFINE2(print_state_changes, pid_t, pid, int, milliseconds)
-{
-  struct task_struct *p;
-  printk("SYSCALL print state changes for process with pid: %d, for the last %d miliseconds\n", pid, milliseconds);
-
-  p = find_task_by_vpid(pid);
-
-  if (p == NULL)
-  {
-    printk("Unable to find process with pid: %d\n", pid);
-    return -1;
-    
-  }
-
-  print_states(p, milliseconds);
 
   return 0;
 }
